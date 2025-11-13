46c4be429919d2e1b54766d7b37bf1e61ebe5f80
israel <israel.is5416@gmail.com>
2025-11-13 03:02:10 -0800

    added pi

--- c++/question 3.cpp
+++ c++/question 3.cpp
@@ -4,10 +4,11 @@ using namespace std;
 
 int main (){
 	double radius;
+	const double pi=3.14;
 	
 	cout<<"enter radius"<<endl;
 	cin>>radius;
-	cout<<"the circumference is "<<2*3.14*radius<<endl;
+	cout<<"the circumference is "<<2*pi*radius<<endl;
 	
 	return 0;
 }
