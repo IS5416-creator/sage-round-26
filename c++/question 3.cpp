814eb919fae5e7c9fff71baf7c2f1a6b6d2c8a50
israel <israel.is5416@gmail.com>
2025-11-13 02:58:30 -0800

    question 3

---
+++ c++/question 3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	double radius;
+	
+	cout<<"enter radius"<<endl;
+	cin>>radius;
+	cout<<"the circumference is "<<2*3.14*radius<<endl;
+	
+	return 0;
+}
