b69ca9a269990888f15994269fb08e2329fef3b5
israel <israel.is5416@gmail.com>
2025-11-19 01:50:03 -0800

    vowel and consonant

---
+++ c++/alphabet.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+string alphabet(char letter){
+	if(letter=='a' || letter=='e' || letter=='i' || letter=='o' || letter=='u'){
+		return "vowel";
+	}else {
+		return "consonant";
+	}
+}
+int main(){
+	cout<<alphabet('f');
+
+}
