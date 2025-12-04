961321086f08d9a1572345dc9f366b19dbb5981e
IS5416 <israel.is5416.com>
2025-12-04 02:53:33 -0800

    javascript

---
+++ javascript/JS/script.js
@@ -0,0 +1,13 @@
+document.getElementById('demo').innerText="hi from java";
+document.getElementById('demo').innerHTML="<h1>hi there<h1>";
+document.getElementById('demo').style.color="red";
+
+const x=54;
+var y;
+
+
+y=634;
+
+console.log(x+y);
+
+document.getElementById('demo').innerText= x+y;
